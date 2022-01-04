8917402b577a2ff04b05a7d5d5e299740ba96dde
frustak <soulburn007@gmail.com>
2022-01-04 23:12:40 +0330

    Implement button component

---
+++ src/lib/components/button.svelte
@@ -0,0 +1,7 @@
+<button
+	class="bg-stone-700 px-2 py-1 relative text-stone-50 rounded flex-grow flex-shrink-0 font-semibold hover:bg-stone-800 hover:text-stone-100"
+	on:click
+>
+	<div class="absolute w-full h-full border-t border-l border-stone-700 -top-1 -left-1 rounded" />
+	<slot />
+</button>
