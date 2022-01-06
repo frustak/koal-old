5c11fcde8592569682aa906678e545ce5bfb4ea9
frustak <soulburn007@gmail.com>
2022-01-06 10:57:30 +0330

    Refactor theme store to use persist

--- src/lib/stores/theme.ts
+++ src/lib/stores/theme.ts
@@ -1,13 +1,8 @@
-import { writable } from 'svelte/store';
+import { persist } from './persist';
 
 export enum Theme {
 	Light = 'light',
 	Dark = 'dark'
 }
 
-export const theme = writable<Theme>(Theme.Light);
-
-export function changeTheme(which: Theme): void {
-	theme.set(which);
-	localStorage.setItem('theme', which);
-}
+export const theme = persist<Theme>('theme', Theme.Light);
