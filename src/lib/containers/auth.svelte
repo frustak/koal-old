618bfe4abe6e8faf7caf521edef216a0a5709c87
frustak <soulburn007@gmail.com>
2022-01-10 23:52:05 +0330

    Implement auth component

---
+++ src/lib/containers/auth.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import AuthForm, { OnSubmitAuth } from '$lib/containers/auth-form.svelte';
+
+	export let title: string;
+	export let onSubmit: OnSubmitAuth;
+	export let link: { label: string; to: string };
+</script>
+
+<section class="max-w-md mx-auto flex flex-col gap-y-10">
+	<h2 class="font-bold text-3xl">{title}</h2>
+	<AuthForm submitText="Sign in" {onSubmit} />
+
+	<a href={link.to} class="link self-end">{link.label}</a>
+</section>
