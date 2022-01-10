d20acf64b2137edadedb53702d26bc3404866d55
frustak <soulburn007@gmail.com>
2022-01-10 23:50:59 +0330

    Remove button component

--- src/lib/components/button.svelte
+++
@@ -1,7 +0,0 @@
-<button
-	class="relative flex-grow flex-shrink-0 px-2 py-1 font-semibold rounded bg-primary/95 text-background/95 hover:bg-primary hover:text-background focus:bg-primary focus:text-background"
-	on:click
->
-	<div class="absolute w-full h-full border-t border-l rounded border-primary -top-1 -left-1" />
-	<slot />
-</button>
