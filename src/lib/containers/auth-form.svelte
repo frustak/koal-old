2a41a1e5645fccb3dca04944a597f124b9da34dd
frustak <soulburn007@gmail.com>
2022-01-10 23:51:55 +0330

    Implement auth form

---
+++ src/lib/containers/auth-form.svelte
@@ -0,0 +1,34 @@
+<script context="module" lang="ts">
+	export type OnSubmitAuth = (values: { email: string; password: string }) => void;
+</script>
+
+<script lang="ts">
+	export let submitText: string;
+	export let onSubmit: OnSubmitAuth;
+
+	let email: string;
+	let password: string;
+</script>
+
+<form
+	class="flex flex-col gap-y-10 bg-primary/5 py-12 px-16 rounded-2xl"
+	on:submit|preventDefault={() => onSubmit({ email, password })}
+>
+	<label class="label">
+		Email
+		<input placeholder="example@mail.com" type="email" class="input" required bind:value={email} />
+	</label>
+	<label class="label">
+		Password
+		<input
+			placeholder="at least 8 characters"
+			type="password"
+			class="input"
+			required
+			minlength={8}
+			bind:value={password}
+		/>
+	</label>
+
+	<button class="button" type="submit">{submitText}</button>
+</form>
