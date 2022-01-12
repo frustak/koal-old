9ca797b13ff60262a6adbef2de777da189abaa13
frustak <soulburn007@gmail.com>
2022-01-12 23:23:39 +0330

    Minor refactor

--- src/lib/components/button.svelte
+++ src/lib/components/button.svelte
@@ -1,13 +1,15 @@
 <script lang="ts">
+	type Type = 'button' | 'submit' | 'reset' | 'menu';
+
 	export let isLoading: boolean = false;
-	export let type: string;
+	export let type: Type;
 </script>
 
 <button
 	class="button px-6 disabled:cursor-not-allowed"
 	class:animate-pulse={isLoading}
-	{type}
 	disabled={isLoading}
+	{type}
 >
 	{#if isLoading}
 		<i class="bi bi-arrow-clockwise inline-block animate-spin" />
--- src/lib/containers/header.svelte
+++ src/lib/containers/header.svelte
@@ -3,8 +3,9 @@
 </script>
 
 <div class="flex justify-between p-4 text-lg">
-	<a class="font-black text-center font-display text-primary/95 hover:text-primary" href="/">Koal</a
-	>
+	<a class="font-black text-center font-display text-primary/95 hover:text-primary" href="/">
+		Koal
+	</a>
 
 	<div class="space-x-6">
 		<button class="button-icon" on:click={changeTheme} title="theme">
