e8b195f80f89ee20aea6e137fc146b634a47d23b
frustak <soulburn007@gmail.com>
2022-01-12 18:56:49 +0330

    Implement button component

---
+++ src/lib/components/button.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	export let isLoading: boolean = false;
+	export let type: string;
+</script>
+
+<button
+	class="button px-6 disabled:cursor-not-allowed"
+	class:animate-pulse={isLoading}
+	{type}
+	disabled={isLoading}
+>
+	{#if isLoading}
+		<i class="bi bi-arrow-clockwise inline-block animate-spin" />
+	{:else}
+		<slot />
+	{/if}
+</button>
