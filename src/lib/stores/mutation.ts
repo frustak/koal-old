c334d414ed6bf93a0fe208a49ba2b9f935d917d4
frustak <soulburn007@gmail.com>
2022-01-13 22:02:42 +0330

    Implement new states for mutation

--- src/lib/stores/mutation.ts
+++ src/lib/stores/mutation.ts
@@ -6,8 +6,11 @@ import { writable } from 'svelte/store';
 interface Query<D, E> {
 	data: D | null;
 	error: E | null;
-	isSubmitting: boolean;
+	isLoading: boolean;
 	isError: boolean;
+	isSuccess: boolean;
+	isRecentError: boolean;
+	isRecentSuccess: boolean;
 }
 
 type Mutate<D, P> = (payload: P) => Promise<D>;
@@ -23,30 +26,69 @@ interface Options<D> {
 
 export function mutation<D, P, E = unknown>(
 	callback: Mutate<D, P>,
-	options?: Options<D>
+	options?: Options<D>,
 ): Mutation<D, P, E> {
 	const store = writable<Query<D, E>>({
 		data: null,
 		error: null,
-		isSubmitting: false,
-		isError: false
+		isLoading: false,
+		isError: false,
+		isSuccess: false,
+		isRecentError: false,
+		isRecentSuccess: false,
 	});
 
 	const mutate = async (payload: P) => {
+		store.update((prev) => ({ ...prev, isLoading: true }));
 		try {
-			store.update((prev) => ({ ...prev, isSubmitting: true }));
 			const data = await callback(payload);
-			options?.onSuccess(data);
-			store.update((prev) => ({ ...prev, data, error: null, isError: false }));
+			handleSuccess<D, E>(store, data, options);
 		} catch (error) {
-			store.update((prev) => ({ ...prev, data: null, error: error, isError: true }));
-		} finally {
-			store.update((prev) => ({ ...prev, isSubmitting: false }));
+			handleError<D, E>(store, error);
 		}
 	};
 
 	return {
 		store,
-		mutate
+		mutate,
 	};
+}
+
+function handleSuccess<D, E = unknown>(
+	store: Writable<Query<D, E>>,
+	data: Awaited<D>,
+	options?: Options<D>,
+) {
+	options?.onSuccess(data);
+	store.update((prev) => ({
+		...prev,
+		data,
+		error: null,
+		isError: false,
+		isSuccess: true,
+		isLoading: false,
+		isRecentError: false,
+		isRecentSuccess: true,
+	}));
+
+	setTimeout(() => {
+		store.update((prev) => ({ ...prev, isRecentSuccess: false }));
+	}, 1000);
+}
+
+function handleError<D, E = unknown>(store: Writable<Query<D, E>>, error: E) {
+	store.update((prev) => ({
+		...prev,
+		data: null,
+		error,
+		isError: true,
+		isSuccess: false,
+		isLoading: false,
+		isRecentError: true,
+		isRecentSuccess: false,
+	}));
+
+	setTimeout(() => {
+		store.update((prev) => ({ ...prev, isRecentError: false }));
+	}, 1000);
 }
