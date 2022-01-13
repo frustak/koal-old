4158e21a43e13bf82060a3e7de2b06d644923ef1
frustak <soulburn007@gmail.com>
2022-01-13 22:02:17 +0330

    Implement error and success state for button

--- src/lib/components/button.svelte
+++ src/lib/components/button.svelte
@@ -1,18 +1,24 @@
 <script lang="ts">
 	type Type = 'button' | 'submit' | 'reset' | 'menu';
 
-	export let isLoading: boolean = false;
-	export let type: Type;
+	export let type: Type = 'button';
+	export let isLoading = false;
+	export let isError = false;
+	export let isSuccess = false;
 </script>
 
 <button
-	class="button px-6 disabled:cursor-not-allowed"
+	class="button px-6 disabled:cursor-not-allowed disabled:hover:bg-primary"
 	class:animate-pulse={isLoading}
 	disabled={isLoading}
 	{type}
 >
 	{#if isLoading}
 		<i class="bi bi-arrow-clockwise inline-block animate-spin" />
+	{:else if isError}
+		<i class="bi bi-x-circle" />
+	{:else if isSuccess}
+		<i class="bi bi-check-circle" />
 	{:else}
 		<slot />
 	{/if}
