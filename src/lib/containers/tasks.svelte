a85736f4e168a105d20233ad07fb1955095779df
frustak <soulburn007@gmail.com>
2022-01-18 20:04:52 +0330

    Implement task selection

--- src/lib/containers/tasks.svelte
+++ src/lib/containers/tasks.svelte
@@ -1,34 +1,46 @@
 <script lang="ts">
 	import { persist } from '$lib/stores/persist';
 
-	const tasks = persist<string[]>('tasks', []);
+	interface Task {
+		description: string;
+	}
+
+	const tasks = persist<Task[]>('tasks', []);
 	let newTask = '';
+	let selectedTask: number | null = null;
 
 	function addTask() {
-		if (!newTask) return;
-		$tasks = [...$tasks, newTask];
+		if (!newTask.trim()) return;
+		$tasks = [...$tasks, { description: newTask }];
 		newTask = '';
 	}
 
 	function removeTask(index: number) {
 		$tasks = $tasks.filter((_, i) => i !== index);
 	}
+
+	function selectTask(index: number) {
+		if (selectedTask === index) selectedTask = null;
+		else selectedTask = index;
+	}
 </script>
 
 <section class="space-y-4">
 	<div class="space-y-3 leading-10">
 		{#each $tasks as task, index}
-			<div
-				class="flex items-center justify-between w-full px-2 text-left transition border-2 rounded outline-none cursor-pointer border-primary/75 focus:bg-primary/5 hover:bg-primary/5 gap-x-4"
+			<button
+				class="flex items-center justify-between w-full px-2 text-left bg-opacity-0 border-2 rounded outline-none cursor-pointer border-primary/75 bg-primary gap-x-4 hover:border-primary"
+				class:bg-opacity-10={selectedTask === index}
+				on:click={() => selectTask(index)}
 			>
-				<p>{task}</p>
+				<p>{task.description}</p>
 				<button
-					class="flex transition rounded button-icon text-background bg-primary hover:bg-background hover:text-primary"
+					class="flex rounded button-icon text-background bg-primary hover:bg-background hover:text-primary"
 					on:click={() => removeTask(index)}
 				>
 					<i class="flex p-1 bi bi-x-lg" />
 				</button>
-			</div>
+			</button>
 		{/each}
 	</div>
 
