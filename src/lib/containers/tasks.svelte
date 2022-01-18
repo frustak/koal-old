18ecf955bed5cb7dc134b0b0dfbfb5c233108b28
frustak <soulburn007@gmail.com>
2022-01-18 22:28:29 +0330

    Implement task spent time

--- src/lib/containers/tasks.svelte
+++ src/lib/containers/tasks.svelte
@@ -1,35 +1,47 @@
 <script lang="ts">
-	import { DEFAULT_DURATION } from '$lib/stores/timer';
+	import { timer } from '$lib/stores/timer';
 	import { getDisplayTime } from '$lib/utils/display';
 	import { persist } from '$lib/utils/persist';
 
 	interface Task {
 		description: string;
-		remained: number;
+		spentTime: number;
 	}
 
 	const tasks = persist<Task[]>('tasks', []);
 	let newTask = '';
-	let selectedTask: number | null = null;
+	let selectedTaskIndex: number | null = null;
+
+	timer.subscribe(() => {
+		if (selectedTaskIndex !== null) {
+			$tasks[selectedTaskIndex].spentTime += 1;
+		}
+	});
 
 	function addTask() {
 		if (!newTask.trim()) return;
 		const task: Task = {
 			description: newTask,
-			remained: DEFAULT_DURATION,
+			spentTime: 0,
 		};
 		$tasks = [...$tasks, task];
 		newTask = '';
 	}
 
 	function removeTask(index: number) {
+		if (selectedTaskIndex === index) selectedTaskIndex = null;
 		$tasks = $tasks.filter((_, i) => i !== index);
 	}
 
 	function selectTask(index: number) {
-		if (selectedTask === index) selectedTask = null;
-		else selectedTask = index;
+		if (selectedTaskIndex === index) {
+			selectedTaskIndex = null;
+			return;
+		}
+		selectedTaskIndex = index;
 	}
+
+	$: console.log(selectedTaskIndex);
 </script>
 
 <section class="space-y-4">
@@ -37,18 +49,18 @@
 		{#each $tasks as task, index}
 			<button
 				class="flex items-center justify-between w-full px-2 text-left bg-opacity-0 border-2 rounded outline-none cursor-pointer border-primary/75 bg-primary gap-x-4 hover:border-primary"
-				class:bg-opacity-10={selectedTask === index}
+				class:bg-opacity-10={selectedTaskIndex === index}
 				on:click={() => selectTask(index)}
 			>
-				<p>
-					<span class="rounded bg-primary text-background px-1 text-xs font-black">
-						{getDisplayTime(task.remained)}
+				<div class="flex items-center gap-x-2">
+					<span class="rounded bg-primary text-background px-1 text-xs font-black font-mono">
+						{getDisplayTime(task.spentTime)}
 					</span>
-					<span>{task.description}</span>
-				</p>
+					<p>{task.description}</p>
+				</div>
 				<button
 					class="flex rounded button-icon text-background bg-primary hover:bg-background hover:text-primary"
-					on:click={() => removeTask(index)}
+					on:click|stopPropagation={() => removeTask(index)}
 				>
 					<i class="flex p-1 bi bi-x-lg" />
 				</button>
