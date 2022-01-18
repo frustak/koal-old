82953470c40e08e02014fea740b222991b7b41f9
frustak <soulburn007@gmail.com>
2022-01-18 19:37:53 +0330

    Implement delete task button

--- src/lib/containers/tasks.svelte
+++ src/lib/containers/tasks.svelte
@@ -18,10 +18,17 @@
 <section class="space-y-4">
 	<div class="space-y-3 leading-10">
 		{#each $tasks as task, index}
-			<button
-				class="w-full px-2 text-left border-2 rounded outline-none border-primary/75 focus:bg-primary/5 hover:bg-primary/5"
-				on:click={() => removeTask(index)}>{task}</button
+			<div
+				class="flex items-center justify-between w-full px-2 text-left transition border-2 rounded outline-none cursor-pointer border-primary/75 focus:bg-primary/5 hover:bg-primary/5 gap-x-4"
 			>
+				<p>{task}</p>
+				<button
+					class="flex transition rounded button-icon text-background bg-primary hover:bg-background hover:text-primary"
+					on:click={() => removeTask(index)}
+				>
+					<i class="flex p-1 bi bi-x-lg" />
+				</button>
+			</div>
 		{/each}
 	</div>
 
