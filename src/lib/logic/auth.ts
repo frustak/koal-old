623e82534e238265dd6d9f6cf62b90c571ba3576
frustak <soulburn007@gmail.com>
2022-01-23 21:24:09 +0330

    Set user on auth

--- src/lib/logic/auth.ts
+++ src/lib/logic/auth.ts
@@ -1,8 +1,10 @@
 import { goto } from '$app/navigation';
 import type { AuthData } from '$lib/api/schemas';
+import { user } from '$lib/stores/user';
 import Cookies from 'js-cookie';
 
 export function onAuthSuccess(data: AuthData): void {
+	user.set({ token: data.token });
 	Cookies.set('token', data.token);
 	goto('/');
 }
