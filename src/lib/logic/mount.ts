39a0753aae6ac75e6d845d2d58999a35eff26111
frustak <soulburn007@gmail.com>
2022-01-23 22:42:22 +0330

    Add sentry capture console integration

--- src/lib/logic/mount.ts
+++ src/lib/logic/mount.ts
@@ -1,5 +1,6 @@
 import { user } from '$lib/stores/user';
 import * as Sentry from '@sentry/browser';
+import { CaptureConsole as CaptureConsoleIntegration } from '@sentry/integrations';
 import { Integrations } from '@sentry/tracing';
 import Cookies from 'js-cookie';
 
@@ -12,7 +13,12 @@ export function onMountApp(): void {
 export function startSentry(): void {
 	Sentry.init({
 		dsn: 'https://294d10c394cc496daaaaaa7451b8183a@o1124827.ingest.sentry.io/6163111',
-		integrations: [new Integrations.BrowserTracing()],
+		integrations: [
+			new Integrations.BrowserTracing(),
+			new CaptureConsoleIntegration({
+				levels: ['info', 'warn', 'error', 'debug', 'assert'],
+			}),
+		],
 		tracesSampleRate: 1.0,
 	});
 }
