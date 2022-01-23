4554a2148aaaeebf36da0da420bb85600d252f9e
frustak <soulburn007@gmail.com>
2022-01-23 22:25:46 +0330

    Add sentry to project

--- src/lib/logic/mount.ts
+++ src/lib/logic/mount.ts
@@ -1,7 +1,18 @@
 import { user } from '$lib/stores/user';
+import * as Sentry from '@sentry/browser';
+import { Integrations } from '@sentry/tracing';
 import Cookies from 'js-cookie';
 
 export function onMountApp(): void {
+	startSentry();
 	const token = Cookies.get('token');
 	if (token) user.set({ token });
+}
+
+export function startSentry(): void {
+	Sentry.init({
+		dsn: 'https://294d10c394cc496daaaaaa7451b8183a@o1124827.ingest.sentry.io/6163111',
+		integrations: [new Integrations.BrowserTracing()],
+		tracesSampleRate: 1.0,
+	});
 }
