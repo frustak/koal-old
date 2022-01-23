0e4b18c9ad7b69ea2af2fb321cd7119ce154a33f
frustak <soulburn007@gmail.com>
2022-01-23 21:37:55 +0330

    Implement profile link

--- src/lib/containers/header.svelte
+++ src/lib/containers/header.svelte
@@ -1,5 +1,11 @@
 <script lang="ts">
 	import { changeTheme } from '$lib/stores/theme';
+	import { user } from '$lib/stores/user';
+	import { derived } from 'svelte/store';
+
+	const profileLink = derived([user], (user) => {
+		return user ? '/profile' : '/sign-in';
+	});
 </script>
 
 <div class="flex justify-between p-4 text-lg">
@@ -11,7 +17,7 @@
 		<button class="button-icon" on:click={changeTheme} title="theme">
 			<i class="bi bi-segmented-nav" />
 		</button>
-		<a class="button-icon" title="account" href="/sign-in">
+		<a class="button-icon" title="account" href={$profileLink}>
 			<i class="bi bi-person-circle" />
 		</a>
 	</div>
