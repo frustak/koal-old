59fabd7a49722232b56922319f86f3423d403649
frustak <soulburn007@gmail.com>
2022-01-25 23:48:16 +0330

    Refactor

--- src/lib/api/request.ts
+++ src/lib/api/request.ts
@@ -1,14 +1,17 @@
 import Cookies from 'js-cookie';
+import type { BeforeRequestHook } from 'ky';
 import ky from 'ky';
 
 const API_URL = import.meta.env.VITE_API_URL;
 
-function addAuthorizationHeader(request: Request) {
+const addAuthorizationHeader: BeforeRequestHook = (request) => {
 	const token = Cookies.get('token');
 	if (token) request.headers.set('Authorization', `Bearer ${token}`);
-}
+};
 
 export const request = ky.create({
 	prefixUrl: API_URL,
-	hooks: { beforeRequest: [addAuthorizationHeader] },
+	hooks: {
+		beforeRequest: [addAuthorizationHeader],
+	},
 });
