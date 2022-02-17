31b5ee18d7979cd997b3697fc42abf72d5a59200
frustak <soulburn007@gmail.com>
2022-02-17 15:33:23 +0330

    Implement field component

---
+++ src/lib/components/field.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	type Type = 'text' | 'email' | 'password';
+
+	export let label: string;
+	export let type: Type = 'text';
+	export let placeholder: string = '';
+	export let name: string;
+	export let required: boolean = false;
+	export let value: string = '';
+	export let minlength: number | undefined = undefined;
+</script>
+
+<label class="flex flex-col text-sm gap-1">
+	{label}
+	<input
+		class="focus:ring-primary focus:border-primary"
+		{placeholder}
+		{name}
+		{required}
+		bind:value
+		{minlength}
+		{...{ type }}
+	/>
+</label>
