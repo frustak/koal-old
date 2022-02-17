4327f3ba9fb511be3af9128ab78d184c40036437
frustak <soulburn007@gmail.com>
2022-02-17 15:33:55 +0330

    Refactor header

--- src/lib/containers/header.svelte
+++ src/lib/containers/header.svelte
@@ -1,24 +1,12 @@
 <script lang="ts">
-	import { changeTheme } from '$lib/stores/theme';
+	import Link from '$lib/components/link.svelte';
 	import { user } from '$lib/stores/user';
-	import { derived } from 'svelte/store';
 
-	const profileLink = derived([user], ([user]) => {
-		return user ? '/profile' : '/sign-in';
-	});
+	$: profileLink = $user ? '/profile' : '/sign-in';
 </script>
 
-<div class="flex justify-between p-4 text-lg">
-	<a class="font-black text-center font-display text-primary/95 hover:text-primary" href="/">
-		Koal
-	</a>
+<div class="flex justify-between py-4 text-lg">
+	<a class="font-black text-center font-display" href="/">Koal</a>
 
-	<div class="space-x-6">
-		<button class="button-icon" on:click={changeTheme} title="theme">
-			<i class="bi bi-segmented-nav" />
-		</button>
-		<a class="button-icon" title="account" href={$profileLink}>
-			<i class="bi bi-person-circle" />
-		</a>
-	</div>
+	<Link to={profileLink}><i class="bi bi-person-circle" /></Link>
 </div>
