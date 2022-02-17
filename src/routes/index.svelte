9901f616936fbe04f8ef091bcd1dfb1b8d673765
frustak <soulburn007@gmail.com>
2022-02-17 15:34:21 +0330

    Refactor home page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,15 +1,7 @@
-<script>
+<script lang="ts">
 	import Tasks from '$lib/containers/tasks.svelte';
-	import Timer from '$lib/containers/timer.svelte';
 </script>
 
 <main>
-	<div class="flex flex-col gap-16 xl:grid xl:grid-cols-3">
-		<div>
-			<Timer />
-		</div>
-		<div class="col-span-2">
-			<Tasks />
-		</div>
-	</div>
+	<Tasks />
 </main>
