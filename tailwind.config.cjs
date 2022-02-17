a187cc7690772022123de723136f53a1f6fdd314
frustak <soulburn007@gmail.com>
2022-02-17 15:32:31 +0330

    Remove extra themes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -24,64 +24,10 @@ const config = {
 					selectors: [':root'],
 					theme: {
 						colors: {
-							primary: '#44403c',
-							background: '#fafaf9',
-							error: '#dc2626',
-						},
-					},
-				},
-				{
-					name: 'gruvbox-light',
-					selectors: ['.gruvbox-light'],
-					theme: {
-						colors: {
-							primary: '#3c3836',
-							background: '#fbf1c7',
-							error: '#cc241d',
-						},
-					},
-				},
-				{
-					name: 'gruvbox',
-					selectors: ['.gruvbox'],
-					theme: {
-						colors: {
-							primary: '#ebdbb2',
-							background: '#282828',
-							error: '#fb4b37',
-						},
-					},
-				},
-				{
-					name: 'void',
-					selectors: ['.void'],
-					theme: {
-						colors: {
-							primary: '#e0aaff',
-							background: '#10002b',
-							error: '#ef4444',
-						},
-					},
-				},
-				{
-					name: 'blue',
-					selectors: ['.blue'],
-					theme: {
-						colors: {
-							primary: '#1d3557',
-							background: '#a8dadc',
-							error: '#991b1b',
-						},
-					},
-				},
-				{
-					name: 'green',
-					selectors: ['.green'],
-					theme: {
-						colors: {
-							primary: '#132a13',
-							background: '#ecf39e',
-							error: '#b91c1c',
+							primary: '#323031',
+							background: '#ffffff',
+							text: '#323031',
+							error: '#db3a34',
 						},
 					},
 				},
