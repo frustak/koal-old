ebbc9a63d1f9d29f62b7b99f4e7d2566b0da5fa4
frustak <soulburn007@gmail.com>
2022-03-16 22:13:18 +0330

    Add new routes

--- src/features/app/routes.ts
+++ src/features/app/routes.ts
@@ -2,6 +2,14 @@ import { lazy } from "solid-js"
 
 export const routes = [
 	{
+		path: "/register",
+		component: lazy(() => import("@pages/register")),
+	},
+	{
+		path: "/login",
+		component: lazy(() => import("@pages/login")),
+	},
+	{
 		path: "/",
 		component: lazy(() => import("@pages/home")),
 	},
