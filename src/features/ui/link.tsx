df2ba24d082d365a56fadbad9293cdbe0f765741
frustak <soulburn007@gmail.com>
2022-03-16 22:12:41 +0330

    Implement link

---
+++ src/features/ui/link.tsx
@@ -0,0 +1,8 @@
+import { Link as RouterLink } from "solid-app-router"
+import { Component, ComponentProps } from "solid-js"
+
+type LinkProps = ComponentProps<typeof RouterLink>
+
+export const Link: Component<LinkProps> = (props) => {
+	return <RouterLink class="hover:underline underline-offset-1" {...props} />
+}
