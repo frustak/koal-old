9d37e2cdcdc70aa500a1afdc275b16b031ced12a
frustak <soulburn007@gmail.com>
2022-03-16 22:11:40 +0330

    Config tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,19 @@
 module.exports = {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
+		fontFamily: {
+			body: ["Patrick Hand", "ui-sans-serif"],
+		},
+		container: {
+			center: true,
+			padding: "5rem",
+		},
+		colors: {
+			primary: "#333333",
+			secondary: "#fefefe",
+			muted: "#767676",
+		},
 		extend: {},
 	},
-	plugins: [],
+	plugins: [require("@tailwindcss/forms")],
 }
