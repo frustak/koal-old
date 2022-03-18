3309bf0da529768567ea8e514a9ed8e409ddadb6
frustak <soulburn007@gmail.com>
2022-03-18 22:01:30 +0330

    Update button component

--- src/features/ui/button.tsx
+++ src/features/ui/button.tsx
@@ -1,9 +1,30 @@
-import { Component } from "solid-js"
+import { Component, mergeProps, Show } from "solid-js"
 
-export const Button: Component = (props) => {
+interface ButtonProps {
+	type?: "button" | "submit"
+	isLoading?: boolean
+}
+
+export const Button: Component<ButtonProps> = (baseProps) => {
+	const props = mergeProps({ type: "button" }, baseProps)
+
 	return (
-		<button class="bg-primary text-secondary flex items-center justify-center rounded h-9 hover:text-primary hover:bg-secondary border border-primary">
-			{props.children}
+		<button
+			type={props.type}
+			class="bg-primary text-secondary flex items-center justify-center rounded h-9 border border-primary transition"
+			classList={{
+				"cursor-wait": props.isLoading,
+				"hover:text-primary hover:bg-secondary": !props.isLoading,
+			}}
+			disabled={props.isLoading}
+		>
+			<Show when={!props.isLoading} fallback={<Loading />}>
+				{props.children}
+			</Show>
 		</button>
 	)
+}
+
+const Loading: Component = () => {
+	return <i class="bi bi-arrow-clockwise animate-spin" />
 }
